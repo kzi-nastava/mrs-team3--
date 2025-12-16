3a4ff0b620309b9a8542b0ffe2677223c878cf3d
BulatovicAndjela <bulatovic.andjelaa@gmail.com>
2025-12-16 16:56:59 +0100

    Feat: add login view

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page';
 import { ProfileComponent } from './profile/profile';
+import { Login } from './login/login';
 
 export const routes: Routes = [
   { path: '', component: LandingPageComponent },
   { path: 'profile', component: ProfileComponent },
+  {path : "login", component: Login},
   { path: '**', redirectTo: '' }
 ];
