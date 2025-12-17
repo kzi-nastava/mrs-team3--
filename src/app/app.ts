711a0d5fc8c48f758270570a234ef62bbbe1dc85
Marko <milutin.sv40.2023@uns.ac.rs>
2025-12-17 23:31:22 +0100

    add feat navigation

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SidebarComponent } from './sidebar/sidebar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SidebarComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class AppComponent {
-  title = 'team3';
-}
+export class AppComponent {}
--- src/app/sidebar/sidebar.ts
+++ src/app/sidebar/sidebar.ts
@@ -1,26 +1,39 @@
 import { Component } from '@angular/core';
-import { NavigationService } from '../services/navigation.service';
+import { Router } from '@angular/router';
+import { CommonModule, NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.css'
 })
 export class SidebarComponent {
-  
-  constructor(private navigationService: NavigationService) {}
 
-  onHome(): void {
-    this.navigationService.navigateToHome();
+  // HARD-CODED for now (easy to replace later)
+  role: 'DRIVER' | 'ADMIN' | 'GUEST' | 'REGISTERED' = 'DRIVER';
+  // role = authService.getRole();
+  constructor(private router: Router) {}
+
+  goHome() {
+    this.router.navigateByUrl('/');
   }
 
-  onMessages(): void {
-    console.log('Messages clicked');
+  goHistory() {
+    this.router.navigateByUrl('/ride-history');
   }
 
-  onProfile(): void {
-    this.navigationService.navigateToProfile();
+  goMessages() {
+    // this.router.navigateByUrl('/messages');
+    alert('Messages - to be implemented');
+  }
+
+  goProfile() {
+    this.router.navigateByUrl('/profile');
+  }
+
+  goLogin() {
+    this.router.navigateByUrl('/login');
   }
 }
