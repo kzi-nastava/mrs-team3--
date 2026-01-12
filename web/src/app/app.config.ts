f651de7923ca1f019332a593d4e91ac2ae682586
BulatovicAndjela <bulatovic.andjelaa@gmail.com>
2026-01-12 17:36:00 +0100

    Feat: reset password token

--- web/src/app/app.config.ts
+++ web/src/app/app.config.ts
@@ -6,8 +6,6 @@ import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeuix/themes/aura';
 import {definePreset} from '@primeuix/themes';
 import {MessageService} from 'primeng/api';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { authInterceptor } from './services/auth.interceptor';
 
 
 const Violet = definePreset(Aura, {
