c8d796e0523bc1c75cb4bf30b3b7b87d6b79b016
Prle04 <prlincevic04@gmail.com>
2026-01-18 21:27:54 +0100

    Feat: add matching driver with a ride

---
+++ backend/uber/Uber/src/main/java/com/st3/uber/enums/RideRejectReason.java
@@ -0,0 +1,9 @@
+package com.st3.uber.enums;
+
+public enum RideRejectReason {
+    NO_ACTIVE_DRIVERS,
+    NO_MATCHING_DRIVERS,
+    NO_DRIVER_WITH_LOCATION,
+    DRIVER_OVER_8_HOURS,
+    ALL_DRIVERS_BUSY
+}
