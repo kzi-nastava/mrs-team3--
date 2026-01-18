250121204680428bb3305f2bf2717edc152112a2
Marko <milutin.sv40.2023@uns.ac.rs>
2026-01-18 01:25:42 +0100

    add driver ride history

--- web/src/app/app.routes.ts
+++ web/src/app/app.routes.ts
@@ -2,12 +2,13 @@ import { Routes } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page';
 import { ProfileComponent } from './profile/profile';
 import { Login } from './login/login';
-import { RideHistoryComponent } from './ride-history/ride-history';
 import { RegisterComponent } from './register/register';
 import { ResetPasswordComponent } from './reset-password/reset-password';
 import { ForgotPassword } from './forgot-password/forgot-password';
 import {VerificationResultComponent} from './verification-result/verification-result';
 import { ErrorComponent } from './error/error';
+import { DriverHistoryComponent } from './ride-history/driver/driver-history';  
+import { PassengerHistoryComponent } from './ride-history/passenger/passenger-history';
 import { authGuard } from './guard/auth.guard';
 export const routes: Routes = [
   { path: '', component: LandingPageComponent },
@@ -23,8 +24,9 @@ export const routes: Routes = [
         .then(m => m.DriverRegisterComponent),
         canActivate: [authGuard]
   },
-
-  { path: 'ride-history', component: RideHistoryComponent, canActivate: [authGuard] },
+  // {path: 'driver-history', component: DriverHistoryComponent, canActivate: [authGuard], data: { role: 'DRIVER' }},
+  {path: 'driver-history', component: DriverHistoryComponent},
+  { path: 'passenger-history', component: PassengerHistoryComponent, canActivate: [authGuard] },
   { path: 'reset-password', component: ResetPasswordComponent },
   { path: 'forgot-password', component: ForgotPassword },
   {path: 'verification-result', component: VerificationResultComponent},
