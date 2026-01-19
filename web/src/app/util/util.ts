e5e42d5a2c465d384b57aa08575922862c38f9c1
BulatovicAndjela <bulatovic.andjelaa@gmail.com>
2026-01-19 03:30:03 +0100

    Fix: github commit and push problem

---
+++ web/src/app/util/util.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-util',
+  imports: [],
+  templateUrl: './util.html',
+  styleUrl: './util.css',
+})
+export class Util {
+
+}
