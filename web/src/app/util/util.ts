198e36c080d66a28a9a321e127011ee7c45a8026
BulatovicAndjela <bulatovic.andjelaa@gmail.com>
2026-01-19 03:30:43 +0100

    Fix: github commit and push problem

--- web/src/app/util/util.ts
+++ web/src/app/util/util.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-util',
@@ -6,6 +6,18 @@ import { Component } from '@angular/core';
   templateUrl: './util.html',
   styleUrl: './util.css',
 })
-export class Util {
 
+export class Util {
+  static async reverseGeocode(lat: number, lng: number): Promise<string> {
+    const url = `https://nominatim.openstreetmap.org/reverse?format=json&lat=${lat}&lon=${lng}`;
+
+    try {
+      const res = await fetch(url, { headers: { Accept: 'application/json' } });
+      const data = await res.json();
+      return data.display_name ?? `${lat}, ${lng}`;
+    } catch (e) {
+      console.error('Reverse geocoding error:', e);
+      return `${lat}, ${lng}`;
+    }
+  }
 }
