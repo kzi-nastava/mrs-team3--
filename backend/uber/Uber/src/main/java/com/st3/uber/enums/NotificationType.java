0f24c524c018bd79a3797edabf372b088ec55d36
Marko <milutin.sv40.2023@uns.ac.rs>
2026-01-21 02:44:40 +0100

    add new dtos

---
+++ backend/uber/Uber/src/main/java/com/st3/uber/enums/NotificationType.java
@@ -0,0 +1,11 @@
+package com.st3.uber.enums;
+
+public enum NotificationType {
+    PROFILE_CHANGE,
+    ACCEPTED_RIDE,
+    DECLINED_RIDE,
+    RIDE_REMINDER,
+    FINISHED_RIDE,
+    PANIC,
+    RIDE_CANCELED
+}
