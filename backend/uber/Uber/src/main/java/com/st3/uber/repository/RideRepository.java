0b9288821f1821a4d9927d6bb61357a9e2ad7c54
BulatovicAndjela <bulatovic.andjelaa@gmail.com>
2026-01-27 22:04:40 +0100

    Feat: panic button logic

--- backend/uber/Uber/src/main/java/com/st3/uber/repository/RideRepository.java
+++ backend/uber/Uber/src/main/java/com/st3/uber/repository/RideRepository.java
@@ -30,6 +30,7 @@ public interface RideRepository extends JpaRepository<Ride, Long> {
     List<Ride> findByStatusAndScheduledAtIsNotNull(RideStatus status);
 
     List<Ride> findByScheduledAtBefore(LocalDateTime dateTime);
+    List<Ride> findByCreatorAndFinishedAtIsNotNull(Passenger creator);
 
     List<Ride> findByStatusAndScheduledAtBetween(
             RideStatus status,
