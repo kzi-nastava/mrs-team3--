ceb10e82fe531cece8469e3c241a4d88a3c242fc
BulatovicAndjela <bulatovic.andjelaa@gmail.com>
2026-01-27 09:36:19 +0100

    feat: init current ride

---
+++ web/src/app/current-ride/current-ride.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-current-ride',
+  imports: [],
+  templateUrl: './current-ride.html',
+  styleUrl: './current-ride.css',
+})
+export class CurrentRide {
+
+}
