08414184a515019c70fa30072b4573db9feff8ab
BulatovicAndjela <bulatovic.andjelaa@gmail.com>
2026-02-06 19:42:53 +0100

    Feat: init admin history

---
+++ web/src/app/ride-history/admin/admin-history.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-history',
+  imports: [],
+  templateUrl: './admin-history.html',
+  styleUrl: './admin-history.css',
+})
+export class AdminHistory {
+
+}
