febfa2762e1e89957dc1ac99a17a4db9ac675a2c
BulatovicAndjela <bulatovic.andjelaa@gmail.com>
2026-02-18 15:21:00 +0100

    Feat: add validators on requests

--- backend/uber/Uber/src/main/java/com/st3/uber/dto/ride/CancelRideRequest.java
+++ backend/uber/Uber/src/main/java/com/st3/uber/dto/ride/CancelRideRequest.java
@@ -1,10 +1,17 @@
 package com.st3.uber.dto.ride;
 
 import com.st3.uber.enums.CancelledBy;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class CancelRideRequest {
+    @NotNull(message = "CancelledBy is required")
     CancelledBy cancelledBy;
+
+    @NotBlank(message = "Cancellation reason is required")
+    @Size(min = 5, max = 255, message = "Cancellation reason must be between 5 and 255 characters")
     String cancellationReason;
 }
--- backend/uber/Uber/src/main/java/com/st3/uber/dto/ride/StopRideRequest.java
+++ backend/uber/Uber/src/main/java/com/st3/uber/dto/ride/StopRideRequest.java
@@ -1,10 +1,13 @@
 package com.st3.uber.dto.ride;
 
 import com.st3.uber.domain.Location;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class StopRideRequest {
+
+  @NotNull(message = "Actual end location is required")
   private Location actualEndLocation;
 
   public StopRideRequest(Long rideId, Location actualEndLocation) {
